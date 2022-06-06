163045771103d0bf0878ccf25b462d1e614be708
bahram <bahram101@mail.ru>
2022-06-06 12:03:02 +0600

    created components, styles

---
+++ src/components/Categories.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+export default function Categories() {
+	const [activeIndex, setActiveIndex] = useState(0);
+
+	const cats = [
+		'Все',
+		'Мясные',
+		'Вегетарианская',
+		'Гриль',
+		'Острые',
+		'Закрытые',
+	];
+
+ 
+	return (
+		<div className='categories'>
+			<ul>
+				{cats.map((cat, i) => (
+					<li
+						key={i}
+						onClick={() => setActiveIndex(i)}
+						className={activeIndex === i ? 'active' : ''}
+					>
+						{cat}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+}
